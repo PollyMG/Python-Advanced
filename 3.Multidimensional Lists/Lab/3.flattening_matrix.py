d91abf4e3ada1f1590ac1bdd3be278e2a46b8b1a
Polly Yankova <pollya@abv.bg>
2021-09-30 21:41:56 +0300

    Multidimensional Lists - Lab  - add Lab 3

---
+++ 3.Multidimensional Lists/Lab/3.flattening_matrix.py
@@ -0,0 +1,8 @@
+n = int(input())
+matrix = []
+for row in range(n):
+    matrix.append([int(x) for x in input().split(', ')])
+flattening_matrix = []
+for row in matrix:
+    flattening_matrix.extend(row)
+print(flattening_matrix)
