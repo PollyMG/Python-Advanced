761279b317bf1190f674cd66ee41b32607a70ab1
Polly Yankova <pollya@abv.bg>
2021-10-01 10:23:44 +0300

    Multidimensional Lists - Lab  - add Lab 4a

--- 3.Multidimensional Lists/Lab/3.flattening_matrix.py
+++ 3.Multidimensional Lists/Lab/3.flattening_matrix.py
@@ -2,7 +2,8 @@ n = int(input())
 matrix = []
 for row in range(n):
     matrix.append([int(x) for x in input().split(', ')])
-flattening_matrix = []
-for row in matrix:
-    flattening_matrix.extend(row)
-print(flattening_matrix)
+# flattening_matrix = []
+# for row in matrix:
+#     flattening_matrix.extend(row)
+# print(flattening_matrix)
+print([x for row in matrix for x in row])
--- 3.Multidimensional Lists/Lab/4.sum_matrix_columns.py
+++ 3.Multidimensional Lists/Lab/4.sum_matrix_columns.py
@@ -4,8 +4,15 @@ matrix = []
 for row in range(rows):
     matrix.append([int(x) for x in input().split()])
 
-for col in range(columns):
-    current_sum = 0
-    for row in range(rows):
-        current_sum += matrix[row][col]
-    print(current_sum)
+# for col in range(columns):
+#     current_sum = 0
+#     for row in range(rows):
+#         current_sum += matrix[row][col]
+#     print(current_sum)
+columns_sum = [0] * columns
+for row in range(rows):
+    for col in range(columns):
+        value = matrix[row][col]
+        columns_sum[col] += value
+for el in columns_sum:
+    print(el)
