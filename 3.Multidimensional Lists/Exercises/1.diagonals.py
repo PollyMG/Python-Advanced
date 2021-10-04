38fca6084db9f642f1d737dcbd679ca2d7a8be74
Polly Yankova <pollya@abv.bg>
2021-10-04 13:46:14 +0300

    Multidimensional Lists - Lab  - add Exercise 1

---
+++ 3.Multidimensional Lists/Exercises/1.diagonals.py
@@ -0,0 +1,17 @@
+n = int(input())
+matrix = []
+
+for row in range(n):
+    matrix.append([int(x) for x in input().split(', ')])
+
+primary_diagonal = []
+secondary_diagonal = []
+
+for row in range(n):
+    primary_diagonal.append(matrix[row][row])
+    secondary_diagonal.append(matrix[row][n - 1 - row])
+    
+print(f"Primary diagonal: {', '.join(str(x) for x in primary_diagonal)}. Sum: {sum(primary_diagonal)}")
+print(f"Secondary diagonal: {', '.join(str(x) for x in secondary_diagonal)}. Sum: {sum(secondary_diagonal)}")
+
+
